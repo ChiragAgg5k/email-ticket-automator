b1647b13f0877a81debc05805f22045102c7a94e
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2025-05-29 09:30:18 +0530

    chore: update email parsing logic

--- functions/email-parsed-webhook/src/main.js
+++ functions/email-parsed-webhook/src/main.js
@@ -1,4 +1,14 @@
 export default async ({ req, res, log, error }) => {
+  const { method, url } = req;
+
+  if (method != 'POST' || url !== '/parse-email') {
+    error('Invalid request: ' + method + ' ' + url);
+    return res.json({
+      success: false,
+      message: 'Invalid request',
+    });
+  }
+
   log(req.body);
 
   return res.json({
